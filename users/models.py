58bf2cf1707602b55c01d06e385653e61fde541f
zakhar <grankin-zakhar@mail.ru>
2023-10-07 01:49:44 +0700

    made User phone field nullable

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
 
-    phone = models.CharField(max_length=35, verbose_name='номер телефона')
+    phone = models.CharField(max_length=35, verbose_name='номер телефона', null=True, blank=True)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', null=True, blank=True)
     generated_key = models.CharField(max_length=30, verbose_name='сгенерированный ключ', null=True, blank=True)
     user_key = models.CharField(max_length=30, verbose_name='ключ верификации', null=True, blank=True)
